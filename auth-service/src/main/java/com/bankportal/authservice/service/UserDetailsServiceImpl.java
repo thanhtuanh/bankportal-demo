afadaf9a0dc03486283f7f72036d6205be51a430
Duc Thanh Nguyen <dnguyen@kolb-digital.de>
2025-07-07 08:43:40 +0200

    clean code

--- auth-service/src/main/java/com/bankportal/authservice/service/UserDetailsServiceImpl.java
+++ auth-service/src/main/java/com/bankportal/authservice/service/UserDetailsServiceImpl.java
@@ -1,14 +1,16 @@
-// src/main/java/com/bankportal/authservice/service/UserDetailsServiceImpl.java
 package com.bankportal.authservice.service;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.bankportal.authservice.model.UserEntity;
 import com.bankportal.authservice.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.*;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.stereotype.Service;
 
-import java.util.List;
+import lombok.RequiredArgsConstructor;
+
 
 @Service
 @RequiredArgsConstructor
